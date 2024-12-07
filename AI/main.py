64d8739c57964c21df24c03c557ce9d31b18e919
Leejungs <127173567+Leejungs@users.noreply.github.com>
2024-12-07 17:18:23 +0900

    Delete AI directory

--- AI/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Query
-from typing import Optional
-from recommend import getRecommendFoodList
-
-app = FastAPI()
-
-@app.get("/recommend")
-def getRecommend(user_id: Optional[int] = Query(...), kcal: Optional[int] = Query(...)):
-    return getRecommendFoodList(kcal, user_id)
